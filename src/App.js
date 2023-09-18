6d1288ba48c58fed756b8d3c342dd008f91de2ef
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-09-18 23:49:11 +0100

    navbar settings

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import './App.css';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import SearchIcon from '@mui/icons-material/Search';
-import { ReactComponent as Logo } from '../src/assets/logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navigation/navbar';
 
 function App() {
   return (
     <div className="App">
-      <div className="hearder-logo">
-        <Logo className='header-logo'/>
-      </div>
-
-      <div className="header-search">
-        <input type="text" className='header-search-field'/>
-        <SearchIcon className='search-icon'/>
-      </div>
-
-      <div className="header-nav">
-        <div className="nav-item">
-          <span className="nav-itemLineOne">Hello there!</span>
-          <span className="nav-itemLinetwo">Sign in?</span>
-        </div>
-
-        <div className="nav-item">
-          <span className="nav-itemLineOne">Your</span>
-          <span className="nav-itemLinetwo">Shop</span>
-        </div>
-
-        <div className="nav-item">
-          <ShoppingCartIcon fontSize="large"/>
-          <span className="nav-itemLinetwo">0</span>
-        </div>
-      </div>
+      <Routes>
+        <Route path='/' element={<Navbar/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/navigation/navbar.js
@@ -0,0 +1,43 @@
+import { Fragment, React } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import './navbar.css'
+
+function Navbar() {
+  return (
+    <Fragment>
+      <div className="header">
+        <Link className='header-logo' to='/'>
+          <Logo className='header-logo-image'/>
+        </Link>
+
+        <div className="header-search">
+          <input type="text" className='header-search-field'/>
+          <SearchIcon className='search-icon'/>
+        </div>
+
+        <div className="header-nav">
+          <div className="nav-item">
+            <span className="nav-itemLineOne">Hello there!</span>
+            <span className="nav-itemLinetwo">Sign in?</span>
+          </div>
+
+          <div className="nav-item">
+            <span className="nav-itemLineOne">Your</span>
+            <span className="nav-itemLinetwo">Shop</span>
+          </div>
+
+          <div className="nav-item">
+            <ShoppingCartIcon fontSize="large"/>
+            <span className="nav-itemLinetwo">0</span>
+          </div>
+        </div>
+      </div>
+      <Outlet/>
+    </Fragment>
+  )
+}
+
+export default Navbar
