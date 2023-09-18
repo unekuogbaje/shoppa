6a7cd55ce99ba6c8e39e24e896d9d11d120575a5
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-09-18 22:47:24 +0100

    dashboard without styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
+import { ReactComponent as Logo } from '../src/assets/logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hearder-logo">
+        <Logo className='header-logo'/>
+      </div>
+
+      <div className="header-search">
+        <input type="text" className='header-search-field'/>
+        <SearchIcon className='search-icon'/>
+      </div>
+
+      <div className="header-nav">
+        <div className="nav-item">
+          <span className="nav-itemLineOne">Hello there!</span>
+          <span className="nav-itemLinetwo">Sign in?</span>
+        </div>
+
+        <div className="nav-item">
+          <span className="nav-itemLineOne">Your</span>
+          <span className="nav-itemLinetwo">Shop</span>
+        </div>
+
+        <div className="nav-item">
+          <ShoppingCartIcon fontSize="large"/>
+          <span className="nav-itemLinetwo">0</span>
+        </div>
+      </div>
     </div>
   );
 }
